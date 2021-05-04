9a42773005cc12d81e47e3e2a1ecd49697e2bda6
Mohagoney Moore <78175440+mooremohagoney@users.noreply.github.com>
2021-05-04 01:34:03 -0700

    Doxygen Comments added

--- Noise_Gate.h
+++ Noise_Gate.h
@@ -4,14 +4,27 @@
 #include "Processor.h"
 
 using namespace std;
-
+/**
+* This is a Noise Gate class
+*/
 class Noise_Gate: public Processor{
 
 	int threshold;
 
 public:
 	
+/**
+* Constructor that takes in the threshold of the Noise Gate
+*/
 	Noise_Gate(int threshold);
+
+
+/**
+* Process Buffer
+* @param bufferSize - (insert information about the size)
+* @param buffer - (insert information about buffer)
+*/
 	void processBuffer(unsigned char* buffer, int bufferSize) override;
 
 };
+
