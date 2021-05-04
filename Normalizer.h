8242bd662f8ba5c1117d9cca5d48d8e1b6811110
Mohagoney Moore <78175440+mooremohagoney@users.noreply.github.com>
2021-05-04 01:35:15 -0700

    Doxygen Comments added

--- Normalizer.h
+++ Normalizer.h
@@ -5,9 +5,19 @@
 
 using namespace std;
 
+/**
+* This is a Limiter class
+*/
 class Limiter: public Processor{
 
 public:	
+
+
+/**
+* Process Buffer
+* @param bufferSize - (insert information about the size)
+* @param buffer - (insert information about buffer)
+*/
 
 	void processBuffer(unsigned char* buffer, int bufferSize) override;
 
