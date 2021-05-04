c0c175b1e686af6e8e219819c9c25ba7277c64c7
Mohagoney Moore <78175440+mooremohagoney@users.noreply.github.com>
2021-05-04 01:36:35 -0700

    Doxygen Comment added

--- Processor.h
+++ Processor.h
@@ -4,12 +4,22 @@
 #include <cstdint>
 #include <math>
 
+/**
+* This is a Processor class
+*/
 class Processor{
 
 public:
+
+/**
+* Process Buffer
+* @param bufferSize - (insert information about the size)
+* @param buffer - (insert information about buffer)
+*/
 	virtual void processBuffer(unsigned char* buffer, int bufferSize) = 0;
 	virtual ~Processor() {};
 
 };
 
 #endif // PROCESSOR_H
+
