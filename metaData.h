383504482f214e6404e8ec837fdc9e551d433bbe
Morgan Young <52981150+missmorganyoung@users.noreply.github.com>
2021-05-04 11:16:22 -0700

    creating the functions for Metadata reading

---
+++ metaData.h
@@ -0,0 +1,53 @@
+#include "metaData.h"
+
+MetaData::MetaData(std::ifstream& file){
+  
+  file.read(mdID, sizeof(mdID));
+  file.read((char*)&mdSize, sizeOf(mdSize));
+  buffer.resize(mdSize);
+  fileread(&buffer[0], mdSize);
+  
+}
+
+std::string MetaData:: getMdID() const;{
+
+  std::string dstring;
+  for(char chunk: mdID){
+    
+    dstring.push_back(chunk);
+    
+  }
+  str.push_back('\0');
+  return dstring;
+  
+}
+
+void MetaData:: setMdID(char mdID){
+  
+  this->mdID = mdID;
+  
+}
+
+int MetaData::getMdSize() const{
+  
+  return mdSize;
+  
+}
+
+void MetaData::setMdSize(int mdSize){
+
+  this->mdSize = mdSize;
+  
+}
+
+std:: string MetaData::getBuffer() const{
+  
+  return buffer;
+  
+}
+
+void MetaData::setBuffer(std::string buffer){
+  
+  this->buffer = buffer;
+  
+}
